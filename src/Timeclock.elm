40501cd67879fd6844a333cd6fe3013cb26b0fd9
bbecton <bbecton@babelstreet.com>
2018-12-06 08:47:04 -0600

    Add ticking and begin duration view

--- src/Timeclock.elm
+++ src/Timeclock.elm
@@ -7,29 +7,43 @@ import Html.Events exposing (onClick)
 import Task exposing (Task)
 import Time exposing (..)
 
+
+
 -- Add Time Zone
--- Ticking Clock
 -- Ticking Duration
 
+
 type alias Model =
-    { startTime : Time.Posix
+    { currentTime : Time.Posix
+    , startTime : Time.Posix
     , stopTime : Time.Posix
+    , zone : Time.Zone
     }
 
 
+initialModel : Model
 initialModel =
-    { startTime = Time.millisToPosix 0
+    { currentTime = Time.millisToPosix 0
+    , startTime = Time.millisToPosix 0
     , stopTime = Time.millisToPosix 0
+    , zone = Time.utc
     }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel
+    , Task.perform AdjustTimeZone Time.here
+    )
 
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \flags -> ( initialModel, Cmd.none )
+        { init = \flags -> ( initialModel, Task.perform AdjustTimeZone Time.here )
         , view = view
         , update = update
-        , subscriptions = \model -> Sub.none
+        , subscriptions = subscriptions
         }
 
 
@@ -37,47 +51,81 @@ view : Model -> Html Msg
 view model =
     div []
         [ h1 [] [ text "Time Clock" ]
+        , p [] [ text ("Current Time: " ++ viewHumanTime model.zone model.currentTime) ]
         , button [ onClick ClockInClick ] [ text "Clock In" ]
         , button [ onClick ClockOutClick ] [ text "Clock Out" ]
         , button [ onClick Clear ] [ text "Clear" ]
         , hr [] []
-        , p [] [ text ("Start: " ++ humanTime model.startTime) ]
-        , p [] [ text ("Stop: " ++ humanTime model.stopTime) ]
-        , p [] [ text ("Total Time Worked: " ++ viewTimeWorked model.startTime model.stopTime) ]
+        , p [] [ text ("Start: " ++ viewHumanTime model.zone model.startTime) ]
+        , p [] [ text ("Stop: " ++ viewHumanTime model.zone model.stopTime) ]
+        , viewTimeWorked model.zone model.startTime model.stopTime
         ]
 
 
-humanTime : Posix -> String
-humanTime time =
+humanTime : Time.Zone -> Posix -> ( String, String, String )
+humanTime zone time =
     let
         hour =
-            String.fromInt (Time.toHour utc time)
+            String.fromInt (Time.toHour zone time)
 
         minute =
-            String.fromInt (Time.toMinute utc time)
+            String.fromInt (Time.toMinute zone time)
 
         second =
-            String.fromInt (Time.toSecond utc time)
+            String.fromInt (Time.toSecond zone time)
+    in
+    ( hour, minute, second )
+
+
+viewHumanTime : Time.Zone -> Posix -> String
+viewHumanTime zone time =
+    let
+        ( hour, minute, second ) =
+            humanTime zone time
     in
     hour ++ ":" ++ minute ++ ":" ++ second
 
 
-viewTimeWorked : Time.Posix -> Time.Posix -> String
-viewTimeWorked startTime stopTime =
-    humanTime (Time.millisToPosix (Time.posixToMillis stopTime - Time.posixToMillis startTime))
+localHumanTime : Time.Posix -> String
+localHumanTime time =
+    viewHumanTime utc time
+
+
+viewTimeWorked : Time.Zone -> Time.Posix -> Time.Posix -> Html Msg
+viewTimeWorked zone startTime stopTime =
+    let
+        ( hour, minute, second ) =
+            humanTime utc (Time.millisToPosix (Time.posixToMillis stopTime - Time.posixToMillis startTime))
+    in
+    h2 []
+        [ text "Total Time Worked"
+        , h3 [] [ text ("Hours " ++ hour) ]
+        , h3 [] [ text ("Minutes " ++ minute) ]
+        , h3 [] [ text ("Seconds " ++ second) ]
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 1000 Tick
 
 
 type Msg
-    = Clear
+    = AdjustTimeZone Time.Zone
+    | Clear
     | ClockIn Time.Posix
     | ClockOut Time.Posix
     | ClockInClick
     | ClockOutClick
+    | Tick Time.Posix
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        AdjustTimeZone newZone ->
+            ( { model | zone = newZone }, Cmd.none )
+
         Clear ->
             ( { model | startTime = Time.millisToPosix 0, stopTime = Time.millisToPosix 0 }, Cmd.none )
 
@@ -92,3 +140,6 @@ update msg model =
 
         ClockOut time ->
             ( { model | stopTime = time }, Cmd.none )
+
+        Tick time ->
+            ( { model | currentTime = time }, Cmd.none )
